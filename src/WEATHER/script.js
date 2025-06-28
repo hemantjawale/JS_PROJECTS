5837dea44d73ebdbbc593a77e953c3e8d27a4a17
Hemant <hemantjawale24@gmail.com>
2025-06-28 20:12:26 +0530

    resume from api

---
+++ src/WEATHER/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cityInput = document.querySelector(".city-input");
+    const getWeatherBtn = document.querySelector(".get-weather-btn");
+    const weatherInfo = document.getElementById("weather-info");
+    const cityNameDisplay = document.getElementById("city-name");
+    const temperatureDisplay = document.getElementById("temperature");
+    const descriptionDisplay = document.getElementById("descrp");
+    const errorMessage = document.getElementById("error-msg");
+    const API_KEY="7c8a1a56652992cada7bf6638d5795d2";
+    
+    getWeatherBtn.addEventListener("click", async()=>{
+        const city = cityInput.value.trim();
+        if(!city) return;
+        //it may throw errr
+        //server may be only continent it will take time
+        try {
+         const weatherData =  await fetchWeatherData(city)
+         displayWeatherData()
+        } catch (error) {
+            showError()
+        }
+
+    })
+   async function fetchWeatherData(city){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+   const response =  fetch(url);
+    console.log(typeof(response));
+    console.log(response);
+    
+    
+    }
+    function displayWeatherData(weatherData){
+
+    }
+    function showError(){
+        weatherInfo.classList('hidden')
+        errorMessage.classList.remove('hidden')
+    }
+});
