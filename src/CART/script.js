dcf62b1ec6ae0eb785291be0be45aa19462af5c7
Hemant Jawale <hemantjawale24@gmail.com>
2025-06-29 21:59:43 +0530

    all done

--- src/CART/script.js
+++ src/CART/script.js
@@ -1,8 +1,9 @@
 const btn1 = document.querySelector(".product1btn");
 const btn2 = document.querySelector(".product2btn");
 const btn3 = document.querySelector(".product3btn");
-const calculate = document.querySelector(".calculate");
-
+// const calculate = document.querySelector(".calculate");
+const addproduct=document.querySelector(".productlist")
+const addtocart=document.querySelector(".product_added")
 const p1 = 4999;
 const p2 = 1999;
 const p3 = 2999;
@@ -11,10 +12,19 @@ let totalPrice = 0;
 
 btn1.addEventListener("click", (e) => {
   totalPrice += p1;
+ displayProduct(1);
 });
 btn2.addEventListener("click", (e) => {
   totalPrice += p2;
+  displayProduct(2);
 });
 btn3.addEventListener("click", (e) => {
   totalPrice += p3;
+  displayProduct1(3);
 });
+setInterval(()=>{
+displayProduct(pId){
+
+}
+},100)
+
