fbea1d51379439865cd54c4006618ba4d67fe65a
Hemant Jawale <hemantjawale24@gmail.com>
2025-06-29 15:23:55 +0530

    js started

---
+++ src/CART/script.js
@@ -0,0 +1,7 @@
+const btn1 = document.querySelector(".product1btn");
+const btn2 = document.querySelector(".product2btn");
+const btn3 = document.querySelector(".product3btn");
+
+const p1 = 4999;
+const p2 = 1999;
+const p3 = 2999;
