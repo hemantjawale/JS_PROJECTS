10828e86f82149b212f5d26862066261cd3dd149
Hemant Jawale <hemantjawale24@gmail.com>
2025-06-29 15:44:36 +0530

    products hrml remaning

--- src/CART/script.js
+++ src/CART/script.js
@@ -1,7 +1,20 @@
 const btn1 = document.querySelector(".product1btn");
 const btn2 = document.querySelector(".product2btn");
 const btn3 = document.querySelector(".product3btn");
+const calculate = document.querySelector(".calculate");
 
 const p1 = 4999;
 const p2 = 1999;
 const p3 = 2999;
+
+let totalPrice = 0;
+
+btn1.addEventListener("click", (e) => {
+  totalPrice += p1;
+});
+btn2.addEventListener("click", (e) => {
+  totalPrice += p2;
+});
+btn3.addEventListener("click", (e) => {
+  totalPrice += p3;
+});
